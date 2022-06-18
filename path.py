300faf52ed0973f93ee881da143f3ce4e113471d
jiangyuefeng161 <yuefengjiang161@gamil.com>
2022-06-18 14:08:59 +0800

    各级别边界获取

--- path.py
+++ path.py
@@ -1,3 +1,3 @@
 import osmnx as ox
-G = ox.graph_from_address(address="天津大学, 天津市, 中国", dist=1000, dist_type="bbox",network_type='drive')
-ox.plot_graph(G)
+city = ox.gdf_from_place('New South Wales')
+ox.plot_shape(ox.project_gdf(city))
